5ed20c6747bbd2d81171d750945d09fe164cc11f
svash <s.vashkevych@gmail.com>
2024-10-04 13:31:57 +0300

    Init project with backend frontend

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,11 +1,27 @@
 import { Button } from "@/components/ui/button";
 
-export default function Home() {
+async function getStrapiData(path: string) {
+  const baseUrl = "http://localhost:1337/";
+  try {
+    const response = await fetch(baseUrl + path);
+    const data = await response.json();
+    return data;
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+export default async function Home() {
+  const strapiData = await getStrapiData("api/home-page");
+  const { Title, description } = strapiData.data;
+  console.log(Title, description);
   return (
     <div>
       <Button size={"sm"} className="w-full">
         Button
       </Button>
+      <h1>{Title}</h1>
+      <p>{description}</p>
     </div>
   );
 }
