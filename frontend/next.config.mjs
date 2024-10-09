82a448adad02f4d9da6432ae8a9c2b15767e5e27
svash <s.vashkevych@gmail.com>
2024-10-09 21:02:21 +0300

    update env

--- frontend/next.config.mjs
+++ frontend/next.config.mjs
@@ -11,6 +11,10 @@ const nextConfig = {
         port: "1337",
         pathname: "/uploads/**/*",
       },
+      {
+        protocol: "https",
+        hostname: "/hospitable-sunshine-11be984e09.media.strapiapp.com",
+      },
     ],
   },
 };
